6414ffa4968da5a276db74467292894a7144313e
Miguel <mgroyes22@gmail.com>
2024-04-30 21:59:52 -0500

    cambio de correo

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -34,7 +34,7 @@
             <div class="row justify-content-between">
                 <div class="col-12 col-md d-flex align-items-center">
                     <p class="mb-0 phone"><span class="mailus">Phone no:</span> <a href="#">+00 1234 567</a> or <span
-                            class="mailus">email us:</span> <a href="#">emailsample@email.com</a></p>
+                            class="mailus">email us:</span> <a href="#">info@drinkpoint.com</a></p>
                 </div>
                 <div class="col-12 col-md d-flex justify-content-md-end">
                     <p class="mb-0">Mon - Fri / 9:00-21:00, Sat - Sun / 10:00-20:00</p>
