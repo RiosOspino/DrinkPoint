0ddba51c2848aec8c1dec16a66794dab882877a2
Leidys Rios O <lriosospino@gmail.com>
2024-05-07 07:15:47 -0500

    Lo que hicimos en clase

--- application/controller/usuarioController.php
+++ application/controller/usuarioController.php
@@ -1,24 +1,51 @@
 <?php
-    //Crear la clase para heredar del controlador
+    //crear la clase para heredar del controlador
     class usuarioController extends Controller{
-        //Crear los atributos. normalmente suelen ser en este caso una variable para llamar los modelos
+        //atributos normalmente suelen ser en este caso una variable para llamar los modelos
 
         private $modeloU;
 
-        //Vamos a crear el constructor 
+        //vamos a crear el constructor
         public function __construct(){
-            //Instanciar los modelos necesarios
+            //instanciar los modelos necesarios
             $this->modeloU = $this->loadModel("mdlUsuario");
-        } 
+        }
 
-        //Vamos a crear un metodo para iniciar la sesion
-        public function login()
-    {
-        // load views
-        require APP . 'view/usuarios/login.php';
+        //vamos a crear un método para iniciar sesión 
+        public function login(){
+            //controlar los errores
+            $error = false;
+            //vamos a validar la comunicación con el modelo usuario y el formulario
+            if(isset($_POST['btnLogin'])){
+                $this->modeloU->__SET('usuario',$_POST['txtUser']);
+                $this->modeloU->__SET('clave',$_POST['txtPassword']);
+
+                //lo anterior pasa a un arreglo vacío
+                $_POST=[];
+
+                //con variable vamos a llamar el método del modelo que nos permite validar los datos
+                $validate = $this->modeloU->validateUser();
+
+                //vamos a revisar esa validación
+                if($validate == true){
+                    $_SESSION['SESSION_START'] = true;
+                    $error = false;
+
+                    //vamos a configurar  superglobales para los atributos de la sesión
+                    $_SESSION['Nombres'] = $validate['Nombres'];
+                    $_SESSION['idUsuario'] = $validate['idUsuario'];
+                    $_SESSION['Apellidos'] = $validate['Apellidos'];
+                    $_SESSION['Documento'] = $validate['Documento'];
+                    $_SESSION['Usuario'] = $validate['Usuario'];
+                    $_SESSION['Descripcion'] = $validate['Descripcion'];
+
+                    //después de la validación que me dirija a un admin
+                    header("Location:" . URL . "usuarioController/main");
+                }else{
+                    $error = true;
+                }
+            }
+            require APP . 'view/usuarios/login.php';
+        }
     }
-    }
-
-
-
 ?>
--- application/model/mdlPersona.php
+++ application/model/mdlPersona.php
@@ -1,11 +1,11 @@
 <?php
-//Vamos a crear la clase de este modelo,por lo general se le da como nombre el mismo archivo mdlPersona
+//vamos a crear la clase de este modelo, por lo general se le da como nombre el mismo del archivo mdlPersona
 
 class mdlPersona{
     //agregar los atributos de esta clase
     public $idPersona;
     public $documento;
-    public $idTipodocumento;
+    public $idTipoDocumento;
     public $nombres;
     public $apellidos;
     public $telefono;
@@ -15,32 +15,32 @@ class mdlPersona{
     public $genero;
     public $db;
 
-    //Crear el metodo para fijar los datos
-    public function _SET($atributo,$valor){
-        $this-> $atributo =$valor;
+    //crear el método para fijar los datos
+    public function __SET($atributo,$valor){
+        $this->$atributo = $valor;
     }
 
-    //Metodo para reclamar los datos cuando sean necesarios
-    public function _GET($atributo){
-        return $this-> $atributo;
+    //método para reclamar los datos cuando sean necesarios
+    public function __GET($atributo){
+        return $this->$atributo;
     }
 
-    //Vamos  a crear la conexion a la db
-    public function _construct($db){
-        //Intentar conectar.
+    //crear la conexión a la db
+    public function __construct($db){
+        //intentar conectar
         try{
             $this->db = $db;
         }catch(PDOException $e){
-            exit("Eror al conectar a la base de datos");
+            exit("Error al conectar a la base de datos");
         }
     }
 
-    //Crear el metodo para registar las personas 
+    //crear el método para registrar las personas
     public function registerPerson(){
-        //Vamos a crear una variable que guardara una consulta
+        //vamos a crear una variable que guardará la consulta
         $sql = "INSERT INTO personas(Documento, Nombres, Apellidos, Email, Telefono, Direccion, Genero, FechaNacimiento, idTipoDocumento) VALUES (?,?,?,?,?,?,?,?,?)";
 
-        //Vamos a crear una variable para mantener lista siempre la consulta y poder enviarla o ejecutarla cada que sea llamada.
+        //vamos a crear una variable para mantener lista siempre la consulta y poder enviarla o ejecutarla cada que sea llamada
         $stm = $this->db->prepare($sql);
         $stm->bindParam(1, $this->documento);
         $stm->bindParam(2, $this->nombres);
@@ -50,31 +50,30 @@ class mdlPersona{
         $stm->bindParam(6, $this->direccion);
         $stm->bindParam(7, $this->genero);
         $stm->bindParam(8, $this->fechaNacimiento);
-        $stm->bindParam(9, $this->idTipodocumento);
-        
-        //Respuesta
+        $stm->bindParam(9, $this->idTipoDocumento);
+        //respuesta
         $resultado = $stm->execute();
         return $resultado;
     }
 
-    //Metodo para retornar el id de la ultima persona registrada
+    //método para retornar el id de la última persona registrada
     public function lastIdPerson(){
         $sql = "SELECT MAX(idPersona) AS lastIdPerson FROM personas";
         $query = $this->db->prepare($sql);
         $query->execute();
-        //Respuesta
-        $lastId = $query-> fetchAll(PDO::FETCH_ASSOC);
+        //respuesta
+        $lastId = $query->fetchAll(PDO::FETCH_ASSOC);
         return $lastId;
     }
 
-    //Metodo que nos va permitir ver el listado de los documentos
+    //método que nos va permitir ver el listado de los documentos
     public function getTypeDocument(){
-        //Consulta 
-        $sql = "SELECT idTipoDocumento, Descripcion AS doc FROM tiposdocumento";
+        //consulta
+        $sql = "SELECT idTipoDocumento, Descripcion AS doc FROM tiposdocumentos";
         $query = $this->db->prepare($sql);
         $query->execute();
-        $doc = $query-> fetchAll(PDO::FETCH_ASSOC);
-        return  $doc;
+        $doc = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $doc;
     }
 }
 ?>
