4704df6f008e04444d6a39914a13c1f0a851cbc8
Leidys Rios O <lriosospino@gmail.com>
2024-05-09 09:46:57 -0500

    header

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -60,11 +60,11 @@
               <div class="menu_section">
                 <h3>General</h3>
                 <ul class="nav side-menu">
-                  <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                  <li><a><i class="fa fa-users"></i> Management User <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                      <li><a href="index.html">Dashboard</a></li>
-                      <li><a href="index2.html">Dashboard2</a></li>
-                      <li><a href="index3.html">Dashboard3</a></li>
+                      <li><a href="<?php echo URL; ?>usuarioController/userRegister">Register User</a></li>
+                      <li><a href="#"></a></li>
+                      <li><a href="#">Dashboard3</a></li>
                     </ul>
                   </li>
                   <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
