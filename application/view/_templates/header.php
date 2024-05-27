aca121b5a8ccbe3013cb2328c7726d7c28f3659e
Miguel <mgroyes22@gmail.com>
2024-05-27 09:07:45 -0500

    img user 2

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -207,7 +207,7 @@
               <ul class=" navbar-right">
                 <li class="nav-item dropdown open" style="padding-left: 15px;">
                   <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                    <img src="<?php echo URL;?>gen/production/images/img.jpg" alt=""><?php echo $_SESSION['Nombres'] . ' ' . $_SESSION['Apellidos'] ; ?>
+                    <img src="<?php echo URL;?>gen/production/images/user.png" alt=""><?php echo $_SESSION['Nombres'] . ' ' . $_SESSION['Apellidos'] ; ?>
                   </a>
                   <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item"  href="javascript:;"> Perfiles</a>
